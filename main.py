820cb35ddd36ba808f30959e518d1cfa5242e902
Chris Caras <christopherjcaras@gmail.com>
2017-04-24 20:45:06 -0500

    need permalinks

--- main.py
+++ main.py
@@ -14,15 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
-import jinja2
-import os
+import webapp2, jinja2, os, cgi, re
 
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
+
+#value of the blog's parent, giving a key for an element that doesn't exist
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -35,62 +37,64 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-#First, set up the blog so that the new post form and the post listing are on the same page, as with AsciiChan,
-# and then separate those portions into separate routes, handler classes, and templates.
-#For now, when a user submits a new post, redirect them to the main blog page.
-
-
 class Body(db.Model): #creates an entity that allows you to make a datatype with various properties
-    subject = db.StringProperty(required = True) #String, float, integer properties
+    subject = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True) #date, time, DateTimeProperty... we chose the latter
+#Other allowed properties: Float, Boolean, Integer, Email, Link, PostalAddress
 # (required = True) issues a constraint. So if someone submits to the DB without a title, it'll be an "exception".
 #Constraints are good to prevent bad data.
-    body = db.TextProperty(required = True)  # Can also use EmailProp.. Link... PostalAddress.. Text for longer strings.
-    created = db.DateTimeProperty(auto_now_add = True) #date, time, DateTimeProperty... we chose the latter
 
-#The /blog route displays the 5 most recent posts.
-#To limit the displayed posts in this way, you'll need to filter the query results.
+#The following function was in Udacity solution, but did not make
+#one iota of difference in my blog:
+    #def render(self):
+        #self._render_text = self.content.replace('\n', '<br>')
+        #return render_str("post.html", p = self)
 
 class MainPage(Handler):
-    def render_front(self, subject="", body="", error=""):  #this function creates default text boxes
-        bodies = db.GqlQuery("SELECT * FROM Body ORDER BY created DESC")
-        self.render("mainblog.html", subject=subject, body=body, error=error, bodies=bodies)  #preserves user input
+
+    def render_front(self):
+        bodies = db.GqlQuery("SELECT * FROM Body ORDER BY created DESC").fetch(limit = 5)
+        self.render("mainblog.html", bodies = bodies)
 
     def get(self):
         self.render_front()
 
     def post(self):
         subject = self.request.get("subject")
-        body = self.request.get("body")
+        content = self.request.get("content")
 
-        if subject and body:
-            p = Body(subject = subject, body = body)
+        if subject and content:
+            p = Body(parent = blog_key(), subject = subject, body = content)
             p.put()
-
             self.redirect("/blog")
+            #self.redirect("/blog/%s" % str(p.key().id()))
         else:
             error = "We need both a title and body text!"
-            self.render_front(subject, body, error)
+            self.render("newpost.html", subject=subject, content=content, error=error)
 
-class NewPost(Handler):
+class CreatePost(Handler):
+    def render_front(self, subject="", content="", error=""):  #this function creates default text boxes
+        self.render("newpost.html", subject=subject, content=content, error=error)  #preserves user input
 
     def get(self):
         self.render_front()
 
-    def post(self):
-        self.redirect("/blog")
+class PermaHandler(Handler):
+    def get(self, post_id):
+        key = db.Key.from_path('Body', int(post_id), parent=blog_key())
+        post = db.get(key)
 
-#You're able to submit a new post at the /newpost route/view.
-#After submitting a new post, your app displays the main blog page.
-#Note that, as with the AsciiChan example, you will likely need to refresh the main blog page to see your new post listed.
+        if not post:
+            self.error(404)
+            return
 
-#If either title or body is left empty in the new post form, the form is rendered again,
-#with a helpful error message and any previously-entered content in the same form inputs.
-
-#form input boxes must have the names 'subject' and 'content' in order for the grading script to correctly post to them.
-
-#To get the ID of an entity you just created: obj.key().id()
+        self.render("blogpost.html", post = post)
 
 app = webapp2.WSGIApplication([
     ('/blog', MainPage),
-    ('/newpost', NewPost)
+    ('/blog/newpost', CreatePost),
+    ('/blog/([0-9]+)', PermaHandler)#, #A regex, anything in the parenthesis
+    #will be passed as a parameter for the get or post function for PermaHandler
+    #webapp2.Route(r'/', handler=PermaHandler, name='blog-entry')
 ], debug=True)
